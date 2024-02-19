4ee3aa1d66de2ff2b0a037c1d9751f997d0bca5a
Kasun Umesh <kasunumesh38@gmail.com>
2024-02-20 02:12:48 +0530

    current weather created

--- src/components/CurrentWeather/CurrentWeather.jsx
+++ src/components/CurrentWeather/CurrentWeather.jsx
@@ -2,11 +2,34 @@ import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
 
 function CurrentWeather() {
   return (
-    <div className="bg-[#FFFFFF] shadow rounded-md p-2 flex flex-col w-96">
+    <div className="bg-[#FFFFFF] shadow rounded-md p-2 flex flex-col w-[960px] m-3 z-50">
         <div className="flex justify-between">
             <p className="font-medium text-[1.125rem] text-[#727E8E]">Current Weather</p>
             <div>
                 <ToggleSwitch/>
+            </div>
+        </div>
+        <div className="flex flex-wrap">
+            <div className="flex flex-col m-3 w-[25rem]">
+                <p className="font-semibold text-[1.25rem] text-[#396bae] mb-2">Colombo</p>
+                <div className="flex">
+                    <img src="src/assets/weather/sunny.svg" alt="" className="w-[100px] h-[100px]"/>
+                    <span className="font-extralight text-[7rem] text-[#4a6fa1] ml-5 leading-none">
+                        81
+                        <sub className="bottom-8 text-[6rem]">&deg;</sub>
+                    </span>
+                </div>
+                <p className="text-[1.375rem] text-left text-[#7b98b2] font-medium">few clouds</p>
+            </div>
+
+            <div className="flex flex-col m-8 ml-8">
+                <p className="text-[1.25rem] text-[#4a6fa1]">
+                    Feels like 81
+                    <sup>&deg;</sup>
+                </p>
+                <div className="flex">
+                    <div></div>
+                </div>
             </div>
         </div>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ import Search from "../components/Search/Search"
 function Home() {
 
   return (
-    <div className="flex relative justify-center h-screen bg-gradient-to-b from-gray-50 to-cyan-300 flex-wrap">
+    <div className="flex relative justify-center h-screen bg-gradient-to-b from-gray-50 to-cyan-300 flex-wrap ">
         <img src="src/assets/bg.svg" alt="backgroundImage" className="absolute object-cover h-screen mix-blend-overlay opacity-80"/>
         <Header/>
         <Search/>
