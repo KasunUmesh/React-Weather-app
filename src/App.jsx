6402575f6fdb982e6bd9d8c8f4adc0b5f560a8ba
Kasun Umesh <kasunumesh38@gmail.com>
2024-02-19 12:45:35 +0530

    background created

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
+import Home from "./pages/Home";
+
 function App() {
   return (
-    <h1 className="text-red-300">Hello</h1>
+    <Home/>
   );
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+
+
+function Home() {
+
+
+  return (
+    <div className="flex relative justify-center h-screen bg-gradient-to-b from-gray-50 to-cyan-300">
+        <img src="src/assets/bg.svg" alt="" className="absolute object-cover h-screen mix-blend-overlay opacity-80"/>
+        <div className=" border border-green-600">Home</div>
+    </div>
+  )
+}
+
+export default Home
