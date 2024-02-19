7ead3fed497b434d3de9b7029449b2ed775bdfb3
Kasun Umesh <kasunumesh38@gmail.com>
2024-02-19 23:26:48 +0530

    search input create

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,9 +1,10 @@
+import { IoSearch } from "react-icons/io5";
+
 function Search() {
   return (
-    <div className="relative h-[3.25rem] rounded-[26px] bg-[#FFFFFF] shadow flex items-center m-3" >
-
-        <input type="text" className=" flex-1 ml-1 mr-1 h-[3rem] rounded-[26px] border-none bg-[#FFFFFF] text-[1.125rem] text-[#727E8E] w-96 focus:outline-none placeholder:text-[#6898d5] p-5" placeholder="Search for location"/>
-
+    <div className="relative h-[3.25rem] rounded-[26px] bg-[#FFFFFF] shadow items-center m-3 w-[960px] flex" >
+        <input type="text" className=" flex-1 ml-1 mr-1 h-[3rem] rounded-[26px] border-none bg-[#FFFFFF] text-[1.125rem] text-[#727E8E] focus:outline-none placeholder:text-[#6898d5] p-5 " placeholder="Search for location"/>
+        <IoSearch color="#4a6fa1" size={27} className="flex items-center me-5 cursor-pointer"/>
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import Header from "../components/Header/Header"
 
+import Search from "../components/Search/Search"
 
 function Home() {
 
-
   return (
-    <div className="flex relative justify-center h-screen bg-gradient-to-b from-gray-50 to-cyan-300 ">
+    <div className="flex relative justify-center h-screen bg-gradient-to-b from-gray-50 to-cyan-300 flex-wrap">
         <img src="src/assets/bg.svg" alt="backgroundImage" className="absolute object-cover h-screen mix-blend-overlay opacity-80"/>
         <Header/>
+        <Search/>
     </div>
   )
 }
